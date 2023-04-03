898f300f89c545ee08c131bd3a742d78941306c6
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2023-04-03 16:08:16 +0500

    [wip] get stats

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -14,7 +14,7 @@ export class BotService {
   getProxyData() {
     axios.default
       .post('http://bot.midera.fun:8000/server/settings', {
-        email: 'krakhimov.it@gmail.com',
+        email: 'vacompany.info@gmail.com',
       })
       .then(async (res) => {
         await validatorDto(BotDto, res.data);
@@ -42,31 +42,65 @@ export class BotService {
       username: proxyData.login,
       password: proxyData.password,
     });
-    await this.page.setViewport({ width: 1200, height: 720 });
+    // await this.page.setViewport({ width: 1200, height: 720 });
     await this.page.goto('https://linkedin.com/login/', {
-      waitUntil: 'networkidle0',
+      waitUntil: 'networkidle2',
       timeout: 0,
     });
-    await this.page.type('input[name="session_key"]', 'krakhimov.it@gmail.com');
-    await this.page.type('input[name="session_password"]', 'kmwd1916');
-    Promise.all([
-      await this.page.click('button[type=submit]'),
-      this.page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 }),
+    await this.page.type(
+      'input[name="session_key"]',
+      'vacompany.info@gmail.com',
+    );
+    await this.page.type('input[name="session_password"]', 'jfhy@u6EW!');
+    await Promise.all([
+      this.page.click('button[type=submit]'),
+      this.page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 }),
     ]);
   }
 
-  startSockets() {
+  async startSockets() {
     this.socket = io('https://bot.midera.fun', {
       transports: ['websocket'],
     });
-    this.socket.on('statistics', ({ link }) => {
-      this.page.goto(link, {
-        waitUntil: 'networkidle0',
-        timeout: 0,
-      });
-      this.socket.emit('statistics', {
-        dima: 'loh',
-      });
+    await this.page.goto('https://linkedin.com/dashboard/', {
+      waitUntil: 'load',
+      timeout: 0,
     });
+    const stats = await this.page.$$('div.pcd-analytics-view-item');
+    const stats_list = [];
+    stats.forEach(async (tool, id) => {
+      console.log(id);
+      const number = await this.page.evaluate(
+        (el) => el.querySelector('div > p.text-body-large-bold').textContent,
+        tool,
+      );
+      const text = await this.page.evaluate(
+        (el) => el.querySelector('div > p.text-body-small').textContent,
+        tool,
+      );
+      stats_list.push({
+        id: id,
+        stat: number.replace(/  |\r\n|\n|\r/gm, ''),
+        text: text.replace(/  |\r\n|\n|\r/gm, ''),
+      });
+      console.log(stats_list);
+    });
+
+    console.log(stats_list);
+
+    // this.socket.on('statistics', ({ link }) => {
+    //   this.page.goto('https://linkedin.com/dashboard/', {
+    //     waitUntil: 'networkidle0',
+    //     timeout: 0,
+    //   });
+    //   this.page.waitForSelector('#ember31');
+    //   const textContent = this.page.evaluate(
+    //     () => document.querySelector('[data-test-foobar3="true"]').textContent,
+    //   );
+    //   console.log('Page title = ' + textContent);
+    //   this.socket.emit('statistics', {
+    //     dima: 'loh',
+    //   });
+    // });
   }
 }
